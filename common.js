02a22a5af4e94fc11d31a42b6f0df16ece93825f
jurr20 <jurr20@gmail.com>
2016-11-07 10:20:14 +0000

    Update common.js

--- common.js
+++ common.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$("#form").submit(function() {
 		$.ajax({
 			type: "POST",
-			url: "https://github.com/jurr20/jurr20.github.io/blob/master/mail.php",
+			url: "https://'+github.com/jurr20/jurr20.github.io/blob/master+'/mail.php",
 			data: $(this).serialize()
 		}).done(function() {
 			$(this).find("input").val("");
