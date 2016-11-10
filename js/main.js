42fc75c54b632e3ce1a05945501d37ef0b2e874f
Daria Dudka <jurr20@gmail.com>
2016-11-10 18:29:58 +0000

    update

---
+++ js/main.js
@@ -0,0 +1,66 @@
+
+$(document).ready(function(){
+
+	var $mainNav = $('.main-menu');
+	$('.mobile-btn').on('click', function() {
+		$(this).toggleClass('active_btn');
+		$mainNav.slideToggle('fast', function(){
+			$mainNav.removeAttr('style').toggleClass('open');
+		});
+	});
+
+// BEGIN Скрипт плавного перехода к нужному блоку
+
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+// END Скрипт плавного перехода к нужному блоку
+
+//start go up
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 900) {
+		$('.js-go-up').fadeIn();
+	} else {
+		$('.js-go-up').fadeOut();
+	}
+});
+
+$('.js-go-up').on("click", function() {
+	$('body,html').animate({
+		scrollTop: 0
+	}, 1500);
+	return true;
+});
+//end go up
+
+//start go up
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 0) {
+		$('.js-go-in').fadeIn();
+	} else {
+		$('.js-go-in').fadeOut();
+	}
+});
+
+$('.js-go-in').on("click", function() {
+	$('body,html').animate({
+		scrollTop: 3000
+	}, 1500);
+	return true;
+});
+//end go up
+
+
+});
+
